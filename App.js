2af1b0aaddb2586fa7e6375f2d148cd741b644ce
Eduard <eduard.tataru@mailo.com>
2023-12-16 12:45:57 +0100

    started to work on getting LDC1.txt and parsing it

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
+import axios from 'axios';
 
 const styles = StyleSheet.create({
   container: {
@@ -20,30 +21,36 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
-  const [tempLDC1, setTempLDC1] = useState("$L’homme refuse d’admettre que beaucoup de ses actions ne viennent pas de son libre arbitre, mais sont des automatismes inscrits génétiquement en lui. Il ne les maîtrisera consciemment qu’en acceptant sa part d'animalité, car accepter par l’esprit sa nature nous rend pur esprit, maîtrisant la matière, maîtrisant le corps. Le seul fait de reconnaître son animalité en ayant la possibilité par l’esprit de s’extérioriser de nous-même pour se regarder avec distance, s’analyser et se comprendre s’appelle « la conscience d’être ». La conscience est donnée par le verbe, et c’est cela qui nous place à part du règne animal. Le fait d’être capable de penser et dire : « Je suis un animal avec des comportements imprimés en moi, et désormais grâce à cette connaissance je vais pouvoir les maîtriser pour maîtriser mon univers. » Qui refuse d’admettre son animalité ne s’élèvera jamais vers la divinité. Ce qui fait l’homme et le rapproche de Dieu, c’est la conscience d’être, qui ne peut émerger que par l’apparition de la parole et des notions de temps et d’espace qu’elle véhicule. L’homme est un singe qui peut parler du passé et du futur, qui peut se parler à lui-même et, ce faisant, il peut enfin parler à Dieu. Au commencement était le verbe, étape fondamentale pour que naisse la conscience et se réveille Dieu. J’étudie les corps et les os depuis trente ans. Ce sont mes recherches qui me poussent à certifier une évolution de l’homme. J’ai étudié l’orientation du deltoïde postérieur de l’homme et de la scapula, et tout confirme son évolution, avec une première fonction du deltoïde postérieur pour le déplacement quadrupède, ensuite une orientation progressive pour tirer le bras vers le bas dans sa fonction arboricole, avant que notre deltoïde postérieur tire le bras vers l’arrière, le centre et le haut, pour équilibrer les déplacements bipèdes et armer les tirs lors des projections. Tout cela, c’est une continuité évolutive, qui va à l’encontre des théories sans preuve des créationnistes fixistes. Personne ne peut prouver que mon travail est faux. Mon travail, c’est ma sueur, je la verse pour l’humanité, et je hais le mensonge.$");
+  const [tempLDC1, setTempLDC1] = useState("#Accepter ce que l’on est  $L’homme refuse d’admettre que beaucoup de ses actions ne viennent pas de son libre  arbitre, mais sont des automatismes inscrits génétiquement en lui. Il ne les maîtrisera  consciemment qu’en acceptant sa part d’animalité, car accepter par l’esprit sa nature nous  rend pur esprit, maîtrisant la matière, maîtrisant le corps.$    #Sur la place de l’homme dans le règne animal  $Le seul fait de reconnaître son animalité en ayant la possibilité par l’esprit de s’extérioriser  de nous-même pour se regarder avec distance, s’analyser et se comprendre s’appelle « la  conscience d’être ». La conscience est donnée par le verbe, et c’est cela qui nous place à  part du règne animal. Le fait d’être capable de penser et dire : « Je suis un animal avec des  comportements imprimés en moi, et désormais grâce à cette connaissance je vais pouvoir  les maîtriser pour maîtriser mon univers. » Qui refuse d’admettre son animalité ne s’élèvera  jamais vers la divinité.$    #Sur la part divine de l’homme  $Ce qui fait l’homme et le rapproche de Dieu c’est la conscience d’être, qui ne peut émerger  que par l’apparition de la parole et des notions de temps et d’espace qu’elle véhicule.  L’homme est un singe qui peut parler du passé et du futur, qui peut se parler à luimême et,  ce étant, il peut enfin parler à Dieu. Au commencement était le verbe, étape fondamentale  pour que naisse la conscience et se réveille Dieu.$");
   const [currentParagraph, setCurrentParagraph] = useState('Hello World!');
+  /*
+  const [LDC1, setLDC1] = useState({[{
+    wordsTitle: 'Accepter ce que on est',
+    title: 'Accepter ce que l’on est',
+    wordsParagraph: [pourquoi, comment, consequence],
+    paragraph: 'L’homme refuse d’admettre que beaucoup de ses actions ne viennent pas de son libre  arbitre, mais sont des automatismes inscrits génétiquement en lui. Il ne les maîtrisera  consciemment qu’en acceptant sa part d’animalité, car accepter par l’esprit sa nature nous  rend pur esprit, maîtrisant la matière, maîtrisant le corps.',
+  }]});
+  */
   const [inputText, setInputText] = useState('');
 
   const handleInputChange = (text) => {
     setInputText(text);
   };
 
-  function textToDictionnary(text) {
-    const words = text.split(' ');
-    const dictionnary = {};
-    words.forEach((word) => {
-      if (dictionnary[word]) {
-        dictionnary[word] += 1;
-      } else {
-        dictionnary[word] = 1;
-      }
-    });
-    console.log(dictionnary);
-    return dictionnary;
+  function openLocalFile() {
+    axios.get('./assets/LDC1.txt')
+      .then(response => {
+        // Handle the response here
+        console.log(response.data);
+      })
+      .catch(error => {
+        // Handle the error here
+        console.error(error);
+      });
   }
 
   useEffect(() => {
-    textToDictionnary(tempLDC1);
+    openLocalFile();
   }, []);
 
   return (
